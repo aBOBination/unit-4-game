36f34c52f92c185fc586e6ab15e41b3b6f525521
Robert Marks <robert.marks54@gmail.com>
2020-03-16 19:55:46 -0600

    objects started

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var player1 = {
+    "name" : "Player1",
+    "healthPoints" : 120,
+    "attackPower" : 8,
+    "counterAttackPower" : 10
+}
+
+var player2 = {
+    "Name" : "Player2",
+    "healthPoints" : 120,
+    "attackPower" : 8,
+    "counterAttackPower" : 10
+}
+
+var player3 = {
+    "Name" : "Player3",
+    "healthPoints" : 120,
+    "attackPower" : 8,
+    "counterAttackPower" : 10
+}
+
+var player4 = {
+    "Name" : "Player4",
+    "healthPoints" : 120,
+    "attackPower" : 8,
+    "counterAttackPower" : 10
+}
