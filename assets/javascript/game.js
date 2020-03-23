9fc8a86a17007fdb9ab6cb841f289d07559519f7
Robert Marks <robert.marks54@gmail.com>
2020-03-22 22:38:46 -0600

    cards almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,21 +9,24 @@ var rpg = {
     reminingOpponents : [],
 
     player1 : {
-        name : "player1",
+        name : "Aragorn",
+        img : "assets/images/aragorn-200.jpg",
         healthPoints : 120,
         attackPower : 8,
         counterAttackPower : 10
     },
 
     player2 : {
-        name : "player2",
+        name : "Legolas",
+        img : "assets/images/legolas-200.png",
         healthPoints : 120,
         attackPower : 8,
         counterAttackPower : 10
     },
 
     player3 : {
-        name : "player3",
+        name : "Gimli",
+        img : "assets/images/gimli-200.png",
         healthPoints : 120,
         attackPower : 8,
         counterAttackPower : 10
@@ -31,6 +34,7 @@ var rpg = {
 
     player4 : {
         name : "player4",
+        img : "assets/images/gimli-200.png",
         healthPoints : 120,
         attackPower : 8,
         counterAttackPower : 10
@@ -40,10 +44,30 @@ var rpg = {
         this.options.push(this.player1, this.player2, this.player3, this.player4);
         for(var i = 0; i < this.options.length; i++){
             var playerCard = $("<div>");
-            playerCard.addClass("player-card options card border");
+            playerCard.addClass("player-card options text-center card border");
             playerCard.attr("id", this.options[i].name)
-            playerCard.text(this.options[i].name)
+            playerCard.attr("style", "max-width:150px")
+
+            var playerImg = $("<img>");
+            playerImg.addClass("card-img-top")
+            playerImg.attr({ "src": this.options[i].img, "alt": "Card image", "style":  "width:100%"});
+
+            var playerBody = $("<div>");
+            playerBody.addClass("card-body")
+
+            var playerName = $("<h5>");
+            playerName.addClass("card-title")
+            playerName.text(this.options[i].name)
+
+            var playerHp = $("<p>");
+            playerHp.text(this.options[i].healthPoints)
+
             $(".selection").append(playerCard);
+            playerCard.append(playerImg)
+            playerCard.append(playerBody)
+            playerBody.append(playerName)
+            playerBody.append(playerHp)
+
         }
     },
 
@@ -54,7 +78,7 @@ var rpg = {
             clicked.removeClass("options")
             clicked.addClass("attacker");
             clicked.appendTo(".attack");
-            $(".options").appendTo(".opponents-remaining")
+            // $(".options").appendTo(".opponents-remaining")
             console.log(attacker)
 
         } else if(this.player === true && this.opponent === false) {
