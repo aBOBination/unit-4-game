13f0c2009ca5fe8f0d4af9817d3800326f44b8cd
Robert Marks <robert.marks54@gmail.com>
2020-03-23 20:44:31 -0600

    cards almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,12 @@ var rpg = {
 
     reminingOpponents : [],
 
+    wins : 0,
+
+    attackHp : 0,
+
+    opponentHp : 0,
+
     player1 : {
         name : "Aragorn",
         img : "assets/images/aragorn-200.jpg",
@@ -19,7 +25,7 @@ var rpg = {
     player2 : {
         name : "Legolas",
         img : "assets/images/legolas-200.png",
-        healthPoints : 120,
+        healthPoints : 100,
         attackPower : 8,
         counterAttackPower : 10
     },
@@ -27,7 +33,7 @@ var rpg = {
     player3 : {
         name : "Gimli",
         img : "assets/images/gimli-200.png",
-        healthPoints : 120,
+        healthPoints : 180,
         attackPower : 8,
         counterAttackPower : 10
     },
@@ -35,7 +41,7 @@ var rpg = {
     player4 : {
         name : "player4",
         img : "assets/images/gimli-200.png",
-        healthPoints : 120,
+        healthPoints : 150,
         attackPower : 8,
         counterAttackPower : 10
     },
@@ -45,8 +51,7 @@ var rpg = {
         for(var i = 0; i < this.options.length; i++){
             var playerCard = $("<div>");
             playerCard.addClass("player-card options text-center card border");
-            playerCard.attr("id", this.options[i].name)
-            playerCard.attr("style", "max-width:150px")
+            playerCard.attr({"style": "max-width:150px", "id": this.options[i].name, "hp": this.options[i].healthPoints})
 
             var playerImg = $("<img>");
             playerImg.addClass("card-img-top")
@@ -67,25 +72,32 @@ var rpg = {
             playerCard.append(playerBody)
             playerBody.append(playerName)
             playerBody.append(playerHp)
-
         }
     },
 
     cardClick : function(clicked){
         if(this.player === false) {
-            var attacker = clicked.attr('id')
+            var hp = clicked.attr('hp')
             this.player = true;
+            this.attackHp = hp
             clicked.removeClass("options")
             clicked.addClass("attacker");
             clicked.appendTo(".attack");
-            // $(".options").appendTo(".opponents-remaining")
-            console.log(attacker)
+            $(".options").addClass("red");
+            console.log(clicked)
 
         } else if(this.player === true && this.opponent === false) {
             this.opponent = true;
+            this.opponentHp = clicked.attr('hp')
             clicked.appendTo(".opponent");
 
         }
+    },
+
+    attackClick : function() {
+        this.attackHp = this.attackHp - 20
+        console.log(this.attackHp)
+        console.log(this.opponentHp)
     }
 }
 
@@ -102,4 +114,5 @@ $(".player-card").on("click", function() {
 // Attack Button listener
 $(".btn").on("click", function() {
     // do stuff
+    rpg.attackClick()
 });
