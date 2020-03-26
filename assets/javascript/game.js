ba7cdc83ffe8024446cd7a6d1ca5e70fc6a64bd4
Robert Marks <robert.marks54@gmail.com>
2020-03-25 22:51:00 -0600

    finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,96 +29,107 @@ var rpg = {
         img : "assets/images/tmnt-raff.jpg",
         healthPoints : 100,
         attackPower : 8,
-        counterAttackPower : 10
+        counterAttackPower : 5
     },
 
     player3 : {
         name : "Donatello",
         img : "assets/images/tmnt-donny.jpg",
-        healthPoints : 180,
+        healthPoints : 150,
         attackPower : 8,
-        counterAttackPower : 10
+        counterAttackPower : 20
     },
 
     player4 : {
         name : "Michelangelo",
         img : "assets/images/tmnt-mickey.jpg",
-        healthPoints : 150,
+        healthPoints : 180,
         attackPower : 8,
-        counterAttackPower : 10
+        counterAttackPower : 25
     },
 
     start : function(){
         this.options.push(this.player1, this.player2, this.player3, this.player4);
         for(var i = 0; i < this.options.length; i++){
             var playerCard = $("<div>");
-            playerCard.addClass("player-card options text-center card border");
-            playerCard.attr({"style": "max-width:150px", "id": this.options[i].name, "hp": this.options[i].healthPoints, "attackPower": this.options[i].attackPower, "counter": this.options[i].counterAttackPower})
+            playerCard.addClass("player-card options text-center card mr-1 ml-1");
+            playerCard.attr({"style": "max-width:150px", "id": this.options[i].name, "hp": this.options[i].healthPoints, "attackPower": this.options[i].attackPower, "counter": this.options[i].counterAttackPower});
 
             var playerImg = $("<img>");
-            playerImg.addClass("card-img-top")
+            playerImg.addClass("card-img-top");
             playerImg.attr({ "src": this.options[i].img, "alt": "Card image", "style":  "width:100%"});
 
             var playerBody = $("<div>");
-            playerBody.addClass("card-body")
+            playerBody.addClass("card-body");
 
             var playerName = $("<h6>");
-            playerName.addClass("card-title")
-            playerName.text(this.options[i].name)
+            playerName.addClass("card-title");
+            playerName.text(this.options[i].name);
 
             var playerHp = $("<p>");
-            playerHp.text(this.options[i].healthPoints)
+            playerHp.addClass("card-hp");
+            playerHp.text(this.options[i].healthPoints);
 
             $(".selection").append(playerCard);
-            playerCard.append(playerImg)
-            playerCard.append(playerBody)
-            playerBody.append(playerName)
-            playerBody.append(playerHp)
+            playerCard.append(playerImg);
+            playerCard.append(playerBody);
+            playerBody.append(playerName);
+            playerBody.append(playerHp);
         }
     },
 
     cardClick : function(clicked){
         if(this.player === false) {
             this.player = true;
-            this.attackHp = clicked.attr('hp')
-            this.attackPower = clicked.attr('attackPower')
-            clicked.removeClass("options")
+            this.attackHp = clicked.attr('hp');
+            this.attackPower = clicked.attr('attackPower');
+            $(".attack").text("You")
+            clicked.removeClass("options");
             clicked.addClass("attacker");
             clicked.appendTo(".attack");
+
             $(".options").addClass("red");
-            $(".message").text("Choose your opponent!")
+            $(".message").text("Choose your opponent!");
 
         } else if(this.player === true && this.opponent === false && clicked.hasClass("attacker") === false) {
             this.opponent = true;
-            this.opponentHp = clicked.attr('hp')
-            this.opponentCounterAttack = clicked.attr('counter')
-
+            this.opponentHp = clicked.attr('hp');
+            this.opponentCounterAttack = clicked.attr('counter');
+            $(".opponent-container").text("Opponent")
             clicked.addClass("opponent");
             clicked.appendTo(".opponent-container");
-            $(".message").text("Fight!!!")
+            $(".message").text("Fight!!!");
+            $(".attack-btn").css("display","inline")
         }
     },
 
     attackClick : function() {
-        if(this.opponent === true) {
+        var attacker = $(".attacker");
+        var opponent = $(".opponent");
+        if(this.opponent === true && this.attackHp > 0) {
         this.opponentHp = this.opponentHp - this.attackPower
         this.attackPower = parseFloat(this.attackPower) + parseFloat($(".attacker").attr('attackPower'))
-        console.log(this.attackPower)
+        $(".fight-message").text("You attached " + opponent.attr('id') + " for " + this.attackPower + " damage!")
         $(".opponent > .card-body > p").text(this.opponentHp)
         if(this.opponentHp <= 0) {
             this.wins++;
             this.opponent = false;
-            $(".opponent-container").empty()
+            $(".opponent").remove()
             if(this.wins === 3) {
+                $(".opponent-container").text("You beat everyone!")
                 $(".message").text("You beat everyone!")
+                $(".fight-message").text("Click reset to play again!")
             } else {
+                $(".opponent-container").text("Choose your next opponent!")
                 $(".message").text("Choose your next opponent!")
             }
         } else {
             this.attackHp = this.attackHp - this.opponentCounterAttack
+            $(".fight-message").html("You attached " + opponent.attr('id') + " for " + this.attackPower + " damage!"+ " <br/> " + opponent.attr('id') + " attacked you back for " + this.opponentCounterAttack + " damage!")
             $(".attacker > .card-body > p").text(this.attackHp)
             if(this.attackHp <= 0) {
                 $(".message").text("You lost!  Click reset to play again.")
+                $(".reset-btn").css("display","inline")
                 }
             }
         }
